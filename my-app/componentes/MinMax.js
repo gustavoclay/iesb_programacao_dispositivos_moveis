fd8519db6c560defd624bd8972720d9fdce6237e
Gustavo Clay do Nascimento Lima <gustavoclay@gmail.com>
2023-08-23 20:29:08 -0300

    Exercicio MinMax

--- my-app/componentes/MinMax.js
+++ my-app/componentes/MinMax.js
@@ -2,15 +2,10 @@ import { Text } from "react-native"
 
 export default function MinMax(props) {
 
-    // const max = props.max;
-    // const min = props.min;
-
     const { min, max } = props;
     console.warn(props);
 
-
-
     return (
-        <Text>{max}</Text>
+        <Text>{max} é maior que {min}</Text>
     )
 }
