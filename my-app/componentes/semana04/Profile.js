620a3ee2792a6f0f91fb4489b9ec28dc55767827
Gustavo Clay do Nascimento Lima <gustavoclay@gmail.com>
2023-09-06 21:35:44 -0300

    Objetos e Listas

--- my-app/componentes/semana04/Profile.js
+++ my-app/componentes/semana04/Profile.js
@@ -1,34 +1,61 @@
 import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 
 export default function Profile({ imgUri, genero, nome, telefone, email }) {
+
+    const pessoa = {
+        imgUri: 'https://www.fakepersongenerator.com/Face/female/female20161024753601562.jpg',
+        genero: 'Feminino',
+        nome: 'Felicia J Ochoa',
+        email: 'aiyana1980@gmail.com',
+        telefone: '630-337-6439'
+    }
+
+    const carros = ['BMW', 'Ferrari', 'Mercedes', 'Audi', 'Fiat']
+
+
     return (
         <View style={styles.container}>
-            <Image
-                source={{ uri: { imgUri } }}
+
+
+            {
+                carros.map(carro => {
+                    return (<Text >{carro}</Text>)
+                })
+            }
+
+            <FlatList
+                data={carros}
+                renderItem={({ item }) => <Text>{item}</Text>}
+
+            />
+
+
+            {/* <Image
+                source={{ uri: pessoa.imgUri }}
                 style={styles.img}
             />
 
-            <View style={styles.labelContainer}>
-                <Text style={[styles.texto, styles.textoLabel]}>Gênero</Text>
-                <Text style={styles.texto}>{genero}</Text>
-            </View>
 
-            <View style={styles.labelContainer}>
-                <Text style={[styles.texto, styles.textoLabel]}>Nome</Text>
-                <Text style={styles.texto}>{nome}</Text>
-            </View>
+            {
+                (pessoa && pessoa.nome) && (
+                    <View style={styles.labelContainer}>
+                        <Text style={[styles.texto, styles.textoLabel]}>Nome</Text>
+                        <Text style={styles.texto}>{pessoa.nome}</Text>
+                    </View>
+                )
+            }
 
 
             <View style={styles.labelContainer}>
                 <Text style={[styles.texto, styles.textoLabel]}>Telefone</Text>
-                <Text style={styles.texto}>{telefone}</Text>
+                <Text style={styles.texto}>{pessoa.telefone}</Text>
             </View>
 
             <View style={styles.labelContainer}>
                 <Text style={[styles.texto, styles.textoLabel]}>Email</Text>
-                <Text style={styles.texto}>{email}</Text>
-            </View>
+                <Text style={styles.texto}>{pessoa.email}</Text>
+            </View> */}
 
         </View>
     )
