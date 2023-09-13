97b7abb42036e12ea171223b19b4325fcde1bced
Gustavo Clay do Nascimento Lima <gustavoclay@gmail.com>
2023-09-13 20:56:01 -0300

    organizando codigo stuff

--- my-app/App.js
+++ my-app/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { ActivityIndicator, Button, MD2Colors } from 'react-native-paper';
-
+import {
+  PaperProvider
+} from 'react-native-paper';
+import Home from './src/screens/home/Home';
 
 
 export default function App() {
 
   return (
-    <View>
-      <ActivityIndicator animating={true} color='red' size={'large'} />
-
-      <Button icon="play" mode="outlined" onPress={() => console.log('Pressed')}>
-      Clique Aqui
-    </Button>
-
-    </View>
+    <PaperProvider>
+      <Home />
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+
 
 
---
+++ my-app/src/styles/defaultStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const cores = {
+    primary: '#2a2a72',
+    secondary: '#ff8f00',
+    white: '#fff',
+    black: '#1c1c1c'
+}
+
+const defaultStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default defaultStyles;
