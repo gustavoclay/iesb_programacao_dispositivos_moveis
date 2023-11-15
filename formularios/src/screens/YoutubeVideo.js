15d7cc4fa95778dc23cb25e383b8accd2a27734c
gustavoclay <gustavoclay@gmail.com>
2023-11-14 21:30:51 -0300

    Adionando uma imagem de fundo em um componente

--- formularios/src/screens/YoutubeVideo.js
+++ formularios/src/screens/YoutubeVideo.js
@@ -1,5 +1,5 @@
-import React, {useState, useCallback } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Button, ImageBackground, StyleSheet, View } from 'react-native';
 import YoutubePlayer from "react-native-youtube-iframe";
 
 export default function YoutubeVideo() {
@@ -18,16 +18,21 @@ export default function YoutubeVideo() {
 
 
     return (
-        <View>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
 
-            <YoutubePlayer
-                height={300}
-                play={playing}
-                videoId={"uY4efoSe-Kc"}
-                onChangeState={onStateChange}
-            />
+            <ImageBackground style={{ flex: 1, justifyContent: 'center' }} resizeMode="cover" source={{ uri: 'https://img.freepik.com/vetores-gratis/design-de-colagem-desenhado-a-mao_23-2149543516.jpg?w=1380&t=st=1700007951~exp=1700008551~hmac=c51650866918a07203f91ae937c59be6494e8fd25528005be442b4eafb39e564' }}>
 
-            <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
+                <YoutubePlayer
+                    height={300}
+                    play={playing}
+                    videoId={"uY4efoSe-Kc"}
+                    onChangeState={onStateChange}
+                />
+
+                <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
+
+            </ImageBackground>
+
         </View>
     )
 }
